c8c98bc50bcc370d96497b546d991b5242d24a0c
Heron Rodrigues <heronrs1@gmail.com>
2020-07-10 21:16:06 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import GlobalStyles from './Styles/global'
+
 function App() {
-  return (
-    <h1>VAMOOOOOOOOOO</h1>
-  );
+    return (
+        <>
+            <h1>VAMOOOOOOOOOO</h1>
+
+            <GlobalStyles />
+        </>
+    );
 }
 
 export default App;
