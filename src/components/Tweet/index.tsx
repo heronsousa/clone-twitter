496aa1042886975ca49ba9bfae8577dd91f9e1ae
Heron Rodrigues <heronrs1@gmail.com>
2020-07-11 21:27:48 -0300

    create Feed component

---
+++ src/components/Tweet/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import {
+    Container,
+    Retweeted,
+    RocketseatIcon,
+    Body,
+    Avatar,
+    Content,
+    Header,
+    Dot,
+    Description,
+    ImageContent,
+    Icons,
+    Status,
+    CommentIcon,
+    RetweetIcon,
+    LikeIcon
+} from './styles';
+
+const Tweet: React.FC = () => {
+  return (
+      <Container>
+          <Retweeted>
+            <RocketseatIcon />
+            Você retweetou
+          </Retweeted>
+
+          <Body>
+              <Avatar />
+
+              <Content>
+                  <Header>
+                      <strong>Rocketseat</strong>
+                      <span>@rocketseat</span>
+                      <Dot />
+                      <time>11 de jul</time>
+                  </Header>
+
+                  <Description>Foguete não tem ré</Description>
+
+                  <ImageContent />
+
+                  <Icons>
+                      <Status>
+                          <CommentIcon />
+                          10
+                      </Status>
+                      <Status>
+                          <RetweetIcon />
+                          16
+                      </Status>
+                      <Status>
+                          <LikeIcon />
+                          666
+                      </Status>
+                  </Icons>
+              </Content>
+          </Body>
+      </Container>
+  );
+}
+
+export default Tweet;
